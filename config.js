55439ccff5fb80c8efebd0f8a3e874248573725b
HC Chen <chceyes@gmail.com>
2013-10-11 23:30:02 +0800

    优化url过滤器，语法风格调整

--- config.js
+++ config.js
@@ -7,14 +7,20 @@
 
 exports.env = 'default';
 
+// 数据库连接路由
 exports.dbAdd = 'mongodb://127.0.0.1:27017/Pophoto';
 
 exports.sys_port = 3001;
 exports.session_secret = 'pophoto';
 exports.session_key = 'pophoto-strong';
 
-exports.thumb = {          //指定缩略图比例
+// 指定缩略图比例
+exports.thumb = {
   width: 200
 };
 
-exports.cookie_maxage = 60 * 60 * 24 * 3; //cookie最大有效期，3天
+// cookie最大有效期，3天
+exports.cookie_maxage = 60 * 60 * 24 * 3;
+
+// 设置过滤url集合
+exports.needFilter = ['/photos.json', '/photo-delete', '/po-photo', '/user'];
