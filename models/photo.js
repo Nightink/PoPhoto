926791ddc6a577e2d9dc7a7e352a8dd3a09574c4
HC Chen <chceyes@gmail.com>
2013-10-12 10:44:36 +0800

    models数据结构调整

--- models/photo.js
+++ models/photo.js
@@ -5,26 +5,37 @@
  * photo数据模型
  */
 
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema;
+var mongoose = require('mongoose');
 
-var PhotoSchema = new Schema({
-  author: { type: String, ref: 'User'},
-  keywords: [String],   //图片关键字
+/**
+ * author: 上传者
+ * keywords : 关键字
+ * reviews : 图片评论记录数组
+ * description : 图片描述
+ * reviews : 图片评论记录数组
+ */
+var PhotoSchema = new mongoose.Schema({
+
+  author: { type: String, ref: 'User' },
+
+  keywords: [String],
   url: String,
   urlSmall: String,
   title: String,
-  description: String,  //图片描述
-  created: { type: Date, default: Date.now },
-  updated: { type: Date, default: Date.now },
-  reviews: [{        //图片评论记录数组
-    author: { type: String, ref: 'User'},
-    content: String,
-    created: { type: Date, default: Date.now }
-  }],
+  description: String,
   width: Number,
   height: Number,
-  type: { type: String, default: "photo" }
-}, { versionKey: false});
 
-mongoose.model('Photo', PhotoSchema);   //创建Photo模型
+  created: { type: Date, default: Date.now },
+  updated: { type: Date, default: Date.now },
+  type: { type: String, default: "photo" },
+
+  reviews: [{
+    author: { type: String, ref: 'User' },
+    content: String,
+    created: { type: Date, default: Date.now }
+  }]
+}, { versionKey: false });
+
+// 创建Photo模型
+mongoose.model('Photo', PhotoSchema);
--- models/user.js
+++ models/user.js
@@ -5,18 +5,25 @@
  * User数据模型
  */
 
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema;
+var mongoose = require('mongoose');
 
-var UserSchema = new Schema({
-  //_id: String,                         //登陆ID
-  username: { type: String, unique: true },           //登陆名称唯一性
-  gender: { type: String, enum: ['男', '女'], default: '男' }, //性别
-  email: String,                        //邮箱
-  password: String,     //密码
-  created: { type: Date, default: Date.now },         //创建时间
-  updated: { type: Date, default: Date.now },    //更新于
-  discipline: String    //专业
-}, { versionKey: false});
+/**
+ * username: 登陆名称
+ * created : 创建时间
+ * updated : 更新时间
+ * discipline : 专业
+ */
+var UserSchema = new mongoose.Schema({
 
-mongoose.model('User', UserSchema);     //创建User用户文档模型
+  username: { type: String, unique: true },
+  gender: { type: String, enum: ['男', '女'], default: '男' },
+  created: { type: Date, default: Date.now },
+  updated: { type: Date, default: Date.now },
+
+  email: String,
+  password: String,
+  discipline: String    
+}, { versionKey: false });
+
+// 创建User用户文档模型
+mongoose.model('User', UserSchema);
