9df1358c376142eaebfb4d9dd98a44075480ab47
Nightink <chceyes@gmail.com>
2013-11-24 15:20:46 +0800

    首页访问处理

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,50 @@
  * PoPhoto index 路由调度接口
  */
 
-var user  = require('./user');
-var photo = require('./photo');
+var mongoose = require('mongoose');
+var _ = require('underscore');
+
+var Photo = mongoose.model('Photo'); 
 
 module.exports = function(app) {
 
   app.get('/', function(req, res) {
 
-    res.redirect('/index.html');
+    var user = req.session.user || req.cookies;
+    console.log('welcome')
+
+    if(_.isNull(user)) {
+
+      return res.json('用户请登陆');
+    }
+
+    var _params = { 
+      limit: 15, 
+      skip: 0, 
+      sort: { updated: -1 } 
+    };
+
+    Photo.find(null, null, _params, function(err, docs) {
+
+      if(err) {
+
+        return res.send(500);
+      }
+      var backDoc = [];
+      _.each(docs, function(doc){
+
+        var reviews = doc.reviews;
+        doc._doc.reviews = ((reviews && reviews instanceof Array) ? reviews.length : 0);
+        backDoc.push(doc._doc);
+      });
+
+      if(user && user.username) {
+        res.render('index', { title: 'PoPhoto', time: Date.now(), user: user, items: backDoc });
+      } else {
+        res.render('index', { title: 'PoPhoto', time: Date.now(), items: backDoc });
+      }
+    });
+
   });
 
   require('./attachment')(app);
