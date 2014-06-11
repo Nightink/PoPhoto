bda47ff40fff71782f01b9a90a18a7dafd929661
Nightink <chceyes@gmail.com>
2014-06-11 19:45:43 +0800

    koajs init

--- Makefile
+++ Makefile
@@ -1,21 +1,28 @@
 
-MOCHA_OPTS= --check-leaks
+MOCHA_OPTS = --check-leaks
 REPORTER = spec
+REGISTRY = --registry=http://registry.npm.taobao.org
+
+install:
+	@npm install $(REGISTRY) --disturl=http://npm.taobao.org/dist
 
 check: test
 
 test: test-unit
 
 test-unit:
-	@NODE_ENV=test ./node_modules/.bin/mocha \
+	@NODE_ENV=test ./node_modules/.bin/mocha --harmony \
 		--require should \
 		--reporter $(REPORTER) \
 		$(MOCHA_OPTS)
 
 debug:
-	@DEBUG=app,app:* NODE_ENV=development node app.js -d -p 3000
+	@DEBUG=app,app:* NODE_ENV=development node --harmony app.js -d -p 3000
 
 run:
-	@NODE_ENV=release node app.js
+	@NODE_ENV=release node --harmony app.js
 
-.PHONY: test test-unit debug run
+watch:
+	@grunt watch
+
+.PHONY: install test test-unit debug run
--- app.js
+++ app.js
@@ -4,14 +4,16 @@
  * 服务器运行
  */
 
-var fs        = require('fs');
-var path      = require('path');
+var fs = require('fs');
+var path = require('path');
 
-var express   = require('express');
-var commander = require('commander');
-var debug     = require('debug')('app');
+var koa = require('koa');
+var router = require('koa-router');
+var bodyParser = require('koa-bodyparser');
+var program = require('commander');
+var debug = require('debug')('app');
 
-commander
+program
   .version(require('./package.json').version)
   .option('-d, --debug', '是否开启前端js debug文件输出', true)
   .option('-p, --port [port]', '设置服务器端口', Number, 3000)
@@ -21,19 +23,9 @@ commander
 // 捕获所有未处理异常
 process.on('uncaughtException', function(err) {
 
-  // 捕获启动端口被占用，异常
-  if (err.code === 'EADDRINUSE') {
-
-    debugging(debug, 'Port %d in use', app.get('port'));
-    app.set('port', randomPort());
-    startServer();
-
-  } else {
-
-    console.log(err.message);
-    console.log(err.stack);
-    process.exit(1);
-  }
+  console.log(err.message);
+  console.log(err.stack);
+  process.exit(1);
 });
 
 // 捕获node 进程结束事件
@@ -42,143 +34,25 @@ process.on('SIGINT', function() {
   process.exit();
 });
 
-var app       = express();
-// require 会进行缓存
-// 针对require 配置，将会导致配置被重写覆盖
-var config    = require('./conf/config.json');
-var debugging = require('./libs/debugging');
+debug('init')
+var app = koa();
 
-debugging(debug, 'app start run');
-
-var tempPath = path.join(__dirname, 'temp');
-// 判断文件夹路径是否存在
-var isDir    = fs.existsSync || path.existsSync;
-
-// 判断是否存在缓存目录，没有则创建缓存目录
-if(!isDir(tempPath)) {
-
-  fs.mkdirSync(tempPath);
-  debugging(debug, 'create image temp dir %s.', tempPath);
-}
-
-// 调整系统congfig  静态文件夹路径
-config.staticPath = commander.static || config.staticPath || '';
-
-// 缓存文件定期处理
-require('./libs/fileClean');
-// 加载视图注册模版
-require('./libs/registerTemplate');
-// 加载实体对象
-require('./models');
-
-function startServer() {
-
-  // 启动服务器,监听端口
-  app.listen(app.get('port'), function(err) {
-
-    if(err) {
-      console.log(err.message);
-      return;
-    }
-
-    debugging(debug, 'Express app server start success http://localhost:%s/', app.get('port'));
-  });
-
-  debugging(debug, 'Express app server listening on port %s', app.get('port'));
-}
-
-// 随机端口轮询
-function randomPort() {
-
-  return Math.floor(Math.random() * 1000) + 7000;
-}
-
-require('./libs/' + config.dbEnv)(app, function(err) {
-
-  if(err) {
-
-    return;
-  }
-
-  // start 全环境下配置
-  // 配置日志记录
-  var stream = fs.createWriteStream(path.join(__dirname, 'info.log'), {
-
-    flags: 'a'
-  });
-  // app.use 内置中间件队列  依次执行队列的中间件
-  app.use(express.logger({stream: stream}));
-  // 添加gzip 输出压缩中间件
-  app.use(express.compress());
-
-  // 配置客户端表单数据提交，必须在app.router之前，否者 res.body 为空
-  app.use(express.methodOverride());
-  // 设置文件上传缓存路径
-  app.use(express.bodyParser({
-    uploadDir: tempPath
-  }));
-
-  // 配置session，必须在cookie之后，依赖cookie
-  app.use(express.cookieParser(config.sessionSecret));
-  app.use(express.session());
-
-  // url为 `*.json` 进行响应头处理
-  app.use(require('./middleware/requestJSONHandler'));
-
-  // 设置过滤器
-  app.use(require('./middleware/filterRouterHandler'));
-
-  // 配置路由异常处理
-  // 路由配置和异常处理必须结合使用，同时必须在表单配置后设置，否则导致表单无法正常使用
-  // 必须配置视图模版路径之前，否则请求index时服务器会直接调用静态路径下index.html
-  app.use(app.router);
-  // app.use(require('./routes')(app));
-  // 设置500服务器处理
-  app.configure('development', function() {
-
-    // 开发环境
-    app.use(express.errorHandler({
-      dumpExceptions: true,
-      showStack: true
-    }));
-
-    debugging(debug, 'development');
-  });
-
-  app.configure('release', function() {
-
-    app.use(require('./middleware/serverErrorHandler'));
-  });
-
-  // 支持字典列表形式显示静态文件目录
-  app.use(express.directory(config.staticPath, { hidden: true }));
-  // 设置静态文件路径
-  app.use(express.static(path.join(config.staticPath)));
-
-  // 设置站点图标
-  app.use(express.favicon(path.join(config.staticPath, 'favicon.ico')));
-
-  // 显示请求错误路由
-  app.use(require('./middleware/routerErrorHandler'));
-
-  // 隐藏响应头`x-powered-by`备注
-  app.disable('x-powered-by');
-  // 配置服务器端口
-  app.set('port', commander.port);
-  // 设置页面渲染类型`*.html`
-  app.set('view engine', 'tpl');
-  // 设置视图模板路径
-  app.set('views', path.join(__dirname, 'views'));
-  // 设置视图渲染引擎
-  app.engine('tpl', require('./middleware/engineHtmlHandler'));
-
-  // 路由调度加载
-  require('./routers')(app);
-  // 进行`sea-config.js`配置输出
-  require('./libs/fileDebug')(commander.debug);
-  // 启动服务器
-  startServer();
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  debug('%s %s - %s', this.method, this.url, ms);
 });
+
+app.use(bodyParser());
+app.use(router(app));
+
+app.get('/', function *index() {
+
+  this.body = 'hello world';
+})
+
+app.listen(program.port);
 
 // `exports test app`
 module.exports = app;
