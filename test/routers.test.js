e3ef2db48c904becba21459975e99af27b9cdc23
Nightink <chceyes@gmail.com>
2014-08-16 23:55:45 +0800

    fix test case

--- test/routers.test.js
+++ test/routers.test.js
@@ -4,25 +4,24 @@ var app = require('../app');
 
 describe('routers/index.js', function () {
 
-  it('should / 200', function (done) {
+  it('should 200 when use page /', function (done) {
     request(app)
     .get('/')
-    .end(function (err, res) {
+    .expect(200, function (err, res) {
       if(err) {
         return done(err);
       }
 
-      res.status.should.equal(200);
       res.type.should.equal('text/html');
       res.text.should.containEql('返回顶部');
       done();
     });
   });
 
-  it('should /index.html 302', function(done) {
+  it('should 200 when use page /index.html', function(done) {
 
     request(app)
     .get('/index.html')
-    .expect(302, done);
+    .expect(200, done);
   });
 });
